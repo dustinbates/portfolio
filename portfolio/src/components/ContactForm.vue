af5c91ef0f35737b29f932754bf112e579b7e25f
Dustin Bates <dustin.bates98@gmail.com>
2023-05-02 17:25:45 -0600

    added email contact form

---
+++ portfolio/src/components/ContactForm.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="flex justify-end text-slate-200 text-shadow text-xl">
+    <form class="flex flex-col w-full" ref="form" @submit.prevent="sendEmail">
+      <label>Name</label>
+      <input type="text" name="name" required
+        class="bg-slate-200 text-black text-sm mb-4 p-2 rounded-sm shadow-sm shadow-sky-600">
+      <label>Email</label>
+      <input type="email" name="email" required
+        class="bg-slate-200 text-black text-sm mb-4 p-2 rounded-sm shadow-sm shadow-sky-600">
+      <label>Message</label>
+      <textarea name="message" required
+        class="bg-slate-200 text-black text-sm mb-4 p-2 rounded-sm shadow-sm shadow-sky-600"></textarea>
+      <div class="flex justify-end">
+        <button type="submit" value="Send"
+          class="text-sm md:text-lg text-shadow border-2 rounded-lg p-3 w-1/3 hover:bg-sky-300 hover:bg-opacity-50 shadow-sm shadow-sky-600">Send</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+
+<script>
+import emailjs from '@emailjs/browser';
+
+export default {
+  methods: {
+    sendEmail() {
+      // @ts-ignore
+      emailjs.sendForm('dustin.bates98', 'template_vjea5hp', this.$refs.form, 'kQqS4s1nbkGYebstt')
+        .then((result) => {
+          console.log('SUCCESS!', result.text);
+        }, (error) => {
+          console.log('FAILED...', error.text);
+        });
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+.text-shadow {
+  text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.583);
+}
+</style>
--- portfolio/src/pages/ContactPage.vue
+++ portfolio/src/pages/ContactPage.vue
@@ -1,18 +1,31 @@
 <template>
-  <div class="w-full bg-gradient-to-b from-sky-500 to-sky-600 rounded-lg overflow-scroll">
-    <h1 class="text-xl bg-slate-300 rounded-lg">This is the Contact Page</h1>
+  <div
+    class="flex flex-col md:flex-row p-12 w-full bg-gradient-to-b from-sky-500 to-sky-600 rounded-lg overflow-scroll text-shadow">
+    <div class="w-full md:w-1/2">
+      <h1 class="text-3xl text-slate-200">Want to get in touch?</h1>
+    </div>
+    <div class="w-full md:w-1/2">
+      <ContactForm />
+    </div>
 
   </div>
 </template>
 
 
 <script>
+import ContactForm from '../components/ContactForm.vue';
+
 export default {
   data() {
-    return {}
-  }
+    return {};
+  },
+  components: { ContactForm }
 }
 </script>
 
 
-<style scoped></style>
+<style scoped>
+.text-shadow {
+  text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.583);
+}
+</style>
